2f86f802a23755caac501f105d813eb8da9eac12
EumT07 <eublanmata@gmail.com>
2024-01-30 08:37:40 -0400

    Admin done

--- src/utils/jsonwebtoken.ts
+++ src/utils/jsonwebtoken.ts
@@ -1,1 +1,19 @@
-import 
+import dotenv from "dotenv"
+import {sign, verify} from "jsonwebtoken";
+
+
+dotenv.config();
+const jwt_secret = process.env.jwt_secret as string;
+
+
+export const generateToken = (id:string) => {
+    const jwt = sign({id}, jwt_secret,{
+        expiresIn: "1h"
+    });
+    return jwt;
+}
+
+export const verifyToken = (token: string)=>{
+    const verified = verify(token,jwt_secret);
+    return verified;
+}
